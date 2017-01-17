26e5bbd1e28b509035b1883e788f53ab7f6fbfbf
rohansingh4 <rohan.singh1104@gmail.com>
2017-01-17 13:17:57 +0530

    Need to see bigger picture here

---
+++ PhraseOMatic.java
@@ -0,0 +1,25 @@
+/*
+
+Phrase-O-Matic
+
+*/
+
+public class PhraseOMatic{
+	public static void main(String[] args) {
+		String[] wordListOne= {"24/7","multi-Tier","30,000 foot","B-to-B","win-win","front-sigma","critical-path","dynamic"};
+		String[] wordListTwo= {"empowered","sticky","value-added","oriented","centric","disributed","clustered","branded","outside-the-box","positioned","networked","focused","leveraged","aligned","targeted","shared","cooperative","accelerated"};
+		String[] wordListThree= {"process","tipping-point","solution","architecture","core competency","strategy","mindshare","portal","space","vision","paradigm","mission"};
+		
+		int oneLenght = wordListOne.length;
+		int twoLenght = wordListTwo.length;
+		int threeLenght = wordListThree.length;
+
+		int rand1 = (int)(Math.random()*oneLenght);
+		int rand2 = (int)(Math.random()*twoLenght);
+		int rand3 = (int)(Math.random()*threeLenght);		
+		
+		String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+		System.out.println("What we need is a " + phrase);
+	}
+}
